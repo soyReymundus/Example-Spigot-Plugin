576faf05cef8ab98f25f7fd78fd3cb4a98def786
Reymundus <53380484+toelf412@users.noreply.github.com>
2020-06-28 23:41:43 -0300

    Mejora en el jar principal

--- src/main/java/com/exampledomi/example/example.java
+++ src/main/java/com/exampledomi/example/example.java
@@ -3,4 +3,12 @@ package com.exampledomi.example;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class example extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
 }
